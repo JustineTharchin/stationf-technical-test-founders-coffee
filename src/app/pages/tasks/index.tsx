1ca9c91d4c1a67c11706d5100c6e0947ba80425d
justine_tharchin <justine.tharchin@acensi.fr>
2023-12-11 16:38:07 +0100

    Initial commit

---
+++ src/app/pages/tasks/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+
+interface Task {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const Tasks: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await response.json();
+        setTasks(data);
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <Link href={`/tasks/${task.id}`}>
+              <a>{task.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Tasks;
